a0c6cf2cc7b92d28b0a83d635171a01396c1f4d7
rusDad <46480184+rusDad@users.noreply.github.com>
2026-03-18 00:05:09 +0300

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -596,7 +596,7 @@ class ContourApp {
         this.canvas.selection = true;
         this.canvas.skipTargetFind = false;
         this.setPanCursor(false);
-        this.clearBrowserSelection();
+        //this.clearBrowserSelection();
 
         if (soft) {
             if (this.pendingPointerResetRenderRaf !== null) {
